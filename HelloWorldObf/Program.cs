a457e303c9914dde2e94e7b4d28b134221fc50c1
yeemi routh <shade4keal@gmail.com>
2021-12-09 21:28:35 +1100

    Add project files.

---
+++ HelloWorldObf/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldObf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{(char)0x48}{(char)0x65}{(char)0x6C}{(char)0x6C}{(char)0x6F}{(char)0x2C}{(char)0x20}{(char)0x57}{(char)0x6F}{(char)0x72}{(char)0x6C}{(char)0x64}{(char)0x21}");
+            Console.Read();
+        }
+    }
+}
